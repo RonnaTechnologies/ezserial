8f7552ad6865784560c023a7a44c93f4b0d37b55
RonnaTechnologies <contact@ronnatech.com>
2023-08-27 11:17:25 +0200

    Improve read example.

--- examples/read.cpp
+++ examples/read.cpp
@@ -1,15 +1,27 @@
 #include <ezserial/ezserial.hpp>
 
 #include <iostream>
+#include <span>
 
-int main()
+int main(int argc, const char** const argv)
 {
-    SerialPort sp{"/dev/ttyACM0"};
-    sp.SetBaudRate(9'600);
+    const auto args = std::span(argv, argc);
+
+    if(args.size() !=  2)
+    {
+        return EXIT_FAILURE;
+    }
+
+    const auto portName = args.back();
+
+    SerialPort sp{portName};
+    sp.SetBaudRate(115'200);
     sp.Open();
     for(std::size_t i = 0; i < 10; ++i)
     {
         std::cout << sp.Read() << std::endl;
     }
     sp.Close();
+
+    return EXIT_SUCCESS;
 }
