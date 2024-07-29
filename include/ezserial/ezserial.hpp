0cfd19197d9240076bf45e09857c4e81b54a8ff7
RonnaTechnologies <contact@ronnatech.com>
2024-07-29 21:12:56 +0200

    Add Poll member function + unix implementation.

--- include/ezserial/ezserial.hpp
+++ include/ezserial/ezserial.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+#include <chrono>
 #include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
 
 class SerialPort
 {
 public:
-
     explicit SerialPort(std::string_view portName);
     virtual ~SerialPort();
 
     void Open() noexcept;
     void Close() noexcept;
+
+    bool Poll(const std::optional<std::chrono::milliseconds>& timeout) noexcept;
 
     void Write(std::string_view data);
     std::string Read();
@@ -23,7 +26,6 @@ public:
 
 
 private:
-
     class SerialPortImpl;
     std::unique_ptr<SerialPortImpl> impl;
 };
--- src/unix.cpp
+++ src/unix.cpp
@@ -1,30 +1,29 @@
+#include <array>
 #include <ezserial/ezserial.hpp>
 
-#include <algorithm>
-#include <cstdio>      // Standard input / output functions
+#include <array>
+#include <cstdio> // Standard input / output functions
 #include <cstdlib>
-#include <cstring>     // String function definitions
-#include <filesystem>
+#include <cstring> // String function definitions
 #include <string>
 #include <unordered_map>
 
-#include <unistd.h>     // UNIX standard function definitions
-#include <fcntl.h>      // File control definitions
-#include <cerrno>      // Error number definitions
-#include <termios.h>    // POSIX terminal control definitions
+#include <cerrno>  // Error number definitions
+#include <fcntl.h> // File control definitions
+#include <sys/epoll.h>
+#include <termios.h> // POSIX terminal control definitions
+#include <unistd.h>  // UNIX standard function definitions
 
 class SerialPort::SerialPortImpl
 {
 
 public:
-
-    explicit SerialPortImpl(std::string_view port) : portName{port} 
+    explicit SerialPortImpl(std::string_view port) : portName{ port }
     {
     }
 
     ~SerialPortImpl()
     {
-
     }
 
     void Open() noexcept
@@ -32,33 +31,32 @@ public:
         handle = ::open(portName.c_str(), O_RDWR | O_NOCTTY);
         termios tty;
 
-
-        if(tcgetattr(handle, &tty) != 0)
+        if (tcgetattr(handle, &tty) != 0)
         {
             return;
         }
 
         const auto baudRateIt = baudRates.find(baudRate);
-        if(baudRateIt == baudRates.cend())
+        if (baudRateIt == baudRates.cend())
         {
             return;
         }
 
         const auto rate = baudRateIt->second;
 
-            // Set baud rate
+        // Set baud rate
         cfsetospeed(&tty, rate);
         cfsetispeed(&tty, rate);
 
-        tty.c_cflag &= ~PARENB;         // Make 8n1 (Disable parity)
-        tty.c_cflag &= ~CSTOPB;         // Use a single stop bit
-        tty.c_cflag &= ~CSIZE;          // Clear all the size bits
-        tty.c_cflag |= CS8;             // 8 bits per byte
+        tty.c_cflag &= ~PARENB; // Make 8n1 (Disable parity)
+        tty.c_cflag &= ~CSTOPB; // Use a single stop bit
+        tty.c_cflag &= ~CSIZE;  // Clear all the size bits
+        tty.c_cflag |= CS8;     // 8 bits per byte
 
-        tty.c_cflag &=  ~CRTSCTS;       // Disable flow control
-        tty.c_cc[VMIN] = 0;             // Read blocks
+        tty.c_cflag &= ~CRTSCTS;       // Disable flow control
+        tty.c_cc[VMIN] = 0;            // Read blocks
         tty.c_cc[VTIME] = 1;           // 1 second read timeout
-        tty.c_cflag |= CREAD | CLOCAL;  // Turn on READ & ignore ctrl lines
+        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines
 
         // Raw mode
         cfmakeraw(&tty);
@@ -66,7 +64,7 @@ public:
         // Flush Port, then applies attributes
         tcflush(this->handle, TCIFLUSH);
 
-        if(tcsetattr(this->handle, TCSANOW, &tty) != 0)
+        if (tcsetattr(this->handle, TCSANOW, &tty) != 0)
         {
             return;
         }
@@ -76,12 +74,30 @@ public:
 
     void Close() noexcept
     {
-        if(isOpen && handle != -1)
+        if (isOpen && handle != -1)
         {
             ::close(handle);
             isOpen = false;
             handle = -1;
         }
+    }
+
+    bool Poll(const std::optional<std::chrono::milliseconds>& timeout) noexcept
+    {
+        std::array<::epoll_event, 1U> e{};
+        if (epoll_handle == -1)
+        {
+            epoll_handle = ::epoll_create1(0);
+            e.front().events |= ::EPOLLRDNORM;
+
+            ::epoll_ctl(epoll_handle, EPOLL_CTL_ADD, handle, e.data());
+        }
+
+        e.front().events = 0;
+        const auto timeout_ms = timeout.has_value() ? timeout->count() : -1;
+        ::epoll_wait(epoll_handle, e.data(), e.size(), timeout_ms);
+
+        return e.front().events | ::EPOLLRDNORM;
     }
 
     void Write(std::string_view data)
@@ -91,73 +107,70 @@ public:
 
     std::string Read()
     {
-        char smallBuff[256] = {0};
+        char smallBuff[256] = { 0 };
         ::read(handle, &smallBuff, sizeof(smallBuff));
-        
-        return std::string{&smallBuff[0]};
-    }
 
+        return std::string{ &smallBuff[0] };
+    }
 
     void SetBaudRate(std::size_t baudRate) noexcept
     {
         this->baudRate = baudRate;
     }
 
-    bool IsOpen() const noexcept { return isOpen; }
+    bool IsOpen() const noexcept
+    {
+        return isOpen;
+    }
 
 private:
-
     std::string portName;
-    bool isOpen{false};
+    bool isOpen{ false };
     std::size_t baudRate;
     int handle{};
+    int epoll_handle{ -1 };
 
-    inline static std::unordered_map<std::size_t, speed_t> baudRates
-    {
-        {0UL, B0},
-        {50UL, B50},
-        {75UL, B75},
-        {110UL, B110},
-        {134UL, B134},
-        {150UL, B150},
-        {200UL, B200},
-        {300UL, B300},
-        {600UL, B600},
-        {1200UL, B1200},
-        {1800UL, B1800},
-        {2400UL, B2400},
-        {4800UL, B4800},
-        {9600UL, B9600},
-        {19200UL, B19200},
-        {38400UL, B38400},
-        {57600UL, B57600},
-        {115200UL, B115200},
-        {230400UL, B230400},
-        {460800UL, B460800},
-        {500000UL, B500000},
-        {576000UL, B576000},
-        {921600UL, B921600},
-        {1000000UL, B1000000},
-        {1152000UL, B1152000},
-        {1500000UL, B1500000},
-        {2000000UL, B2000000},
-        {2500000UL, B2500000},
-        {3000000UL, B3000000},
-        {3500000UL, B3500000},
-        {4000000UL, B4000000},
+    inline static std::unordered_map<std::size_t, speed_t> baudRates{
+        { 0UL, B0 },
+        { 50UL, B50 },
+        { 75UL, B75 },
+        { 110UL, B110 },
+        { 134UL, B134 },
+        { 150UL, B150 },
+        { 200UL, B200 },
+        { 300UL, B300 },
+        { 600UL, B600 },
+        { 1200UL, B1200 },
+        { 1800UL, B1800 },
+        { 2400UL, B2400 },
+        { 4800UL, B4800 },
+        { 9600UL, B9600 },
+        { 19200UL, B19200 },
+        { 38400UL, B38400 },
+        { 57600UL, B57600 },
+        { 115200UL, B115200 },
+        { 230400UL, B230400 },
+        { 460800UL, B460800 },
+        { 500000UL, B500000 },
+        { 576000UL, B576000 },
+        { 921600UL, B921600 },
+        { 1000000UL, B1000000 },
+        { 1152000UL, B1152000 },
+        { 1500000UL, B1500000 },
+        { 2000000UL, B2000000 },
+        { 2500000UL, B2500000 },
+        { 3000000UL, B3000000 },
+        { 3500000UL, B3500000 },
+        { 4000000UL, B4000000 },
     };
-
 };
 
-SerialPort::SerialPort(std::string_view portName)
-: impl{std::make_unique<SerialPortImpl>(portName)}
+SerialPort::SerialPort(std::string_view portName) : impl{ std::make_unique<SerialPortImpl>(portName) }
 {
-
 }
 
 SerialPort::~SerialPort()
 {
-
 }
 
 void SerialPort::Open() noexcept
@@ -168,6 +181,11 @@ void SerialPort::Open() noexcept
 void SerialPort::Close() noexcept
 {
     impl->Close();
+}
+
+bool SerialPort::Poll(const std::optional<std::chrono::milliseconds>& timeout) noexcept
+{
+    return impl->Poll(timeout);
 }
 
 void SerialPort::Write(std::string_view data)
